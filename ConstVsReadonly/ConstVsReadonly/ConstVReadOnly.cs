eaa6dcebb278becf2404e431352272d349ac1199
Kaoxyooj <locksage@hotmail.com>
2016-01-25 11:44:04 -0600

    went through constVReadonly lab

---
+++ ConstVsReadonly/ConstVsReadonly/ConstVReadOnly.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVsReadonly
+{
+    class ConstVReadOnly
+    {
+            public readonly double PI= 3.1;
+            public const int inchesToFeet = 12;
+        public ConstVReadOnly()
+        {
+            this.PI = 3.14;
+        }
+    }
+}
+
+
